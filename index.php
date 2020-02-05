a73568a9244328d7ca235c9976a8c241c1662949
ajhswan <andrewswan.dev@gmail.com>
2020-02-05 10:23:03 +0000

    moved functions into seperate function files, including creating seperate db connection file

--- index.php
+++ index.php
@@ -1,41 +1,18 @@
 <?php
 
-// DB CONNECTION
-$db = new PDO('mysql:host=db; dbname=collectionApp', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+require_once('dbConnect.php');
+require_once('functions.php');
 
-// FETCH DATA FROM DATABASE
+$db = connectToDb($dbName, $userName, $password);
 
-
-function query($query, $db) {
-    $dbQuery = $db->prepare($query);
-    $dbQuery->execute();
-    $result = $dbQuery->fetchAll();
-
-    return $result;
-}
+//set query variable and pass to query function to fetch data from DB
 $sqlQuery = 'SELECT `id`, `supplier_name`, `date`, `amount`,`ccy`, `details`
 FROM `receiptRecord`';
 $result = query($sqlQuery, $db);
 
 
-// function to get data from query array into new data structure
 
-function displayData (array $query) {
-    $result = '';
 
-    foreach($query as $array) {
-        $result .= "<div class='receipt'>";
-        $result .= '<h2> Receipt </h2>';
-        $result .= "<div class='supplier'>" . "<h3>" . $array['supplier_name'] . "</h3>" . "</div>";
-        $result .= "<div class='details'>" . "<h4>" . $array['details'] . "</h4>" . "</div>";
-        $result .= "<div class='amount'>" . "<h4>£" . $array['amount'] . "</h4>" . "</div>";
-        $result .= "<div class='id'>" . "<h5>" . "INV". $array['id'] . "</h5>" . "</div>";
-        $result .= "<div class='date'>" . "<h5>" . $array['date'] . "</h5>" . "</div>";
-        $result .= "</div>";
-    }
-    return $result;
-}
 //echo "<pre>";
 //var_dump($result);
 //echo '</pre>';
